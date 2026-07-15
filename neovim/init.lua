539d7775381318c92b046035675992c6227f0120
StructPoltergeist <124460109+StructPoltergeist@users.noreply.github.com>
2026-07-15 08:52:47 -0600

    Add NeoVim Config

---
+++ neovim/init.lua
@@ -0,0 +1,9 @@
+vim.o.autocomplete = true
+vim.o.confirm = true
+vim.o.laststatus = 2
+vim.o.list = true
+vim.o.number = true
+vim.o.smoothscroll = true
+vim.o.termguicolors = true
+vim.o.wrap = true
+
