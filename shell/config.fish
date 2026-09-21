91b2ca35cdd5569a6681750c08da6a06211cd77e
StructPoltergeist <124460109+StructPoltergeist@users.noreply.github.com>
2026-09-21 12:23:40 -0600

    Update File

--- shell/config.fish
+++ shell/config.fish
@@ -3,7 +3,7 @@
 # Licensed under the GNU Affero General Public License, Version 3.0.
 
 # You may obtain a copy of the License at:
-# https://www.gnu.org/licenses/lgpl-3.0.en.html
+# https://www.gnu.org/licenses/agpl-3.0.en.html
 
 function __starship_set_job_count --description 'Set STARSHIP_JOBS using fish job groups (or legacy PIDs if toggled)'
     # To force legacy behavior (process PIDs), set this variable to "false":
